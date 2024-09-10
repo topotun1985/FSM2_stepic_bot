dd1e694b6d0c90c53974a97462949e81f476ceda
Vadim Black <vvchernyh85@gmail.com>
2024-09-10 12:58:54 +0300

    added redis

--- main.py
+++ main.py
@@ -1,1 +1,271 @@
-#
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state, State, StatesGroup
+from aiogram.fsm.storage.redis import RedisStorage, Redis
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message, PhotoSize
+
+BOT_TOKEN = '7064168249:AAFlr6jFl6fCBH5liH45cqhHqP6GN6-HzBg'
+
+redis = Redis(host='localhost')
+storage = RedisStorage(redis=redis)
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher(storage=storage)
+
+user_dict = {}
+
+class FSMFillForm(StatesGroup):
+    fill_name = State()
+    fill_age = State()
+    fill_gender = State()
+    upload_photo = State()
+    fill_education = State()
+    fill_wish_news = State()
+
+
+@dp.message(Command('start'), StateFilter(default_state))
+async def process_start_command(message: Message):
+    await message.answer(
+        text='Этот бот демонстрирует работу FSM\n\n'
+             'Чтобы перейти к заполнению анкеты - '
+             'отправьте команду /fillform'
+    )
+
+
+@dp.message(Command('cancel'), StateFilter(default_state))
+async def process_cancel_command(message: Message):
+    await message.answer(
+        text='Отменять нечего. Вы вне машины состояний\n\n'
+             'Чтобы перейти к заполнению анкеты - '
+             'отправьте команду /fillform'
+    )
+
+
+@dp.message(Command('cancel'), ~StateFilter(default_state))
+async def process_cancel_command_state(message: Message, state: FSMContext):
+    await message.answer(
+        text='Вы вышли из машины состояний\n\n'
+             'Чтобы снова перейти к заполнению анкеты - '
+             'отправьте команду /fillform'
+
+    )
+    await state.clear()
+
+
+@dp.message(Command('fillform'), StateFilter(default_state))
+async def process_fillform_command(message: Message, state: FSMContext):
+    await message.answer(text='Пожалуйста, введите ваше имя')
+    await state.set_state(FSMFillForm.fill_name)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_name), F.text.isalpha())
+async def process_name_sent(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer(text='Спасибо!\n\nА теперь введите ваш возраст')
+    await state.set_state(FSMFillForm.fill_age)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_name))
+async def warning_not_name(message: Message):
+    await message.answer(
+        text='То, что вы отправили не похоже на имя\n\n'
+             'Пожалуйста, введите ваше имя\n\n'
+             'Если вы хотите прервать заполнение анкеты - '
+             'отправьте команду /cancel'
+    )
+
+
+@dp.message(StateFilter(FSMFillForm.fill_age),
+            lambda x: x.text.isdigit() and 18 <= int(x.text) <= 80)
+async def process_age_sent(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    male_button = InlineKeyboardButton(
+        text='Мужской 👨',
+        callback_data='мужской'
+    )
+    female_button = InlineKeyboardButton(
+        text='Женский 👩',
+        callback_data='женский'
+    )
+    undefined_button = InlineKeyboardButton(
+        text='Я пока не определился 🤷',
+        callback_data='я не знаю'
+    )
+    keyboard = [
+        [male_button, female_button],
+        [undefined_button]
+    ]
+    markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    await message.answer(
+        text='Спасибо!\n\nУкажите ваш пол',
+        reply_markup=markup
+    )
+    await state.set_state(FSMFillForm.fill_gender)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_age))
+async def warning_not_age(message: Message):
+    await message.answer(
+        text='Возраст должен быть целым числом от 18 до 80\n\n'
+             'Попробуйте еще раз\n\nЕсли вы хотите прервать '
+             'заполнение анкеты - отправьте команду /cancel'
+    )
+
+
+@dp.callback_query(StateFilter(FSMFillForm.fill_gender),
+                   F.data.in_(['мужской', 'женский', 'я не знаю']))
+async def process_gender_press(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(gender=callback.data)
+    await callback.message.delete()
+    await callback.message.answer(
+        text='Спасибо! А теперь загрузите, пожалуйста, ваше фото'
+    )
+    await state.set_state(FSMFillForm.upload_photo)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_gender))
+async def warning_not_gender(message: Message):
+    await message.answer(
+        text='Пожалуйста, пользуйтесь кнопками '
+             'при выборе пола\n\nЕсли вы хотите прервать '
+             'заполнение анкеты - отправьте команду /cancel'
+    )
+
+
+@dp.message(StateFilter(FSMFillForm.upload_photo),
+            F.photo[-1].as_('largest_photo'))
+async def process_photo_sent(message: Message, state: FSMContext, largest_photo: PhotoSize):
+    await state.update_data(
+        photo_unique_id=largest_photo.file_unique_id,
+        photo_id=largest_photo.file_id
+    )
+    secondary_button = InlineKeyboardButton(
+        text='Среднее',
+        callback_data='среднее'
+    )
+    higher_button = InlineKeyboardButton(
+        text='Высшее',
+        callback_data='высшее'
+    )
+    no_edu_button = InlineKeyboardButton(
+        text='Нет образования',
+        callback_data='нет образования'
+    )
+    keyboard = [
+        [secondary_button, higher_button],
+        [no_edu_button]
+    ]
+    markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+    await message.answer(
+        text='Спасибо!\n\nУкажите ваше образование',
+        reply_markup=markup
+    )
+    await state.set_state(FSMFillForm.fill_education)
+
+
+@dp.message(StateFilter(FSMFillForm.upload_photo))
+async def warning_not_photo(message: Message):
+    await message.answer(
+        text='Пожалуйста, на этом шаге отправьте '
+             'ваше фото\n\nЕсли вы хотите прервать '
+             'заполнение анкеты - отправьте команду /cancel'
+    )
+
+
+@dp.callback_query(StateFilter(FSMFillForm.fill_education),
+                   F.data.in_(['среднее', 'высшее', 'нет образования']))
+async def process_education_press(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(education=callback.data)
+    yes_news_button = InlineKeyboardButton(
+        text='Да, спасибо!',
+        callback_data='yes_news'
+    )
+    no_news_button = InlineKeyboardButton(
+        text='Нет, спасибо!',
+        callback_data='no_news'
+    )
+    keyboard = [
+        [yes_news_button, no_news_button]
+    ]
+    markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    await callback.message.edit_text(
+        text='Спасибо!\n\nОстался последний шаг.\n'
+             'Хотели бы вы получать новости?',
+        reply_markup=markup
+    )
+    await state.set_state(FSMFillForm.fill_wish_news)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_education))
+async def warning_not_education(message: Message):
+    await message.answer(
+        text='Пожалуйста, пользуйтесь кнопками при выборе образования\n\n'
+             'Если вы хотите прервать заполнение анкеты - отправьте '
+             'команду /cancel'
+    )
+
+
+@dp.callback_query(StateFilter(FSMFillForm.fill_wish_news),
+                   F.data.in_(['yes_news', 'no_news']))
+async def process_wish_news_press(callback: CallbackQuery, state: FSMContext):
+    # Cохраняем данные о получении новостей по ключу "wish_news"
+    if callback.data == 'yes_news':
+        await state.update_data(wish_news='Да')
+    elif callback.data == 'no_news':
+        await state.update_data(wish_news='Нет')
+    # Добавляем в "базу данных" анкету пользователя
+    # по ключу id пользователя
+    user_dict[callback.from_user.id] = await state.get_data()
+    # Завершаем машину состояний
+    await state.clear()
+    # Отправляем в чат сообщение о выходе из машины состояний
+    await callback.message.edit_text(
+        text='Спасибо! Ваши данные сохранены!\n\n'
+             'Вы вышли из машины состояний'
+    )
+    # Отправляем в чат сообщение с предложением посмотреть свою анкету
+    await callback.message.answer(
+        text='Чтобы посмотреть данные вашей '
+             'анкеты - отправьте команду /showdata'
+    )
+
+
+@dp.message(StateFilter(FSMFillForm.fill_wish_news))
+async def warning_not_wish_news(message: Message):
+    await message.answer(
+        text='Пожалуйста, воспользуйтесь кнопками!\n\n'
+             'Если вы хотите прервать заполнение анкеты - '
+             'отправьте команду /cancel'
+    )
+
+
+@dp.message(Command(commands='showdata'), StateFilter(default_state))
+async def process_showdata_command(message: Message):
+    # Отправляем пользователю анкету, если она есть в "базе данных"
+    if message.from_user.id in user_dict:
+        await message.answer_photo(
+            photo=user_dict[message.from_user.id]['photo_id'],
+            caption=f'Имя: {user_dict[message.from_user.id]["name"]}\n'
+                    f'Возраст: {user_dict[message.from_user.id]["age"]}\n'
+                    f'Пол: {user_dict[message.from_user.id]["gender"]}\n'
+                    f'Образование: {user_dict[message.from_user.id]["education"]}\n'
+                    f'Получать новости: {user_dict[message.from_user.id]["wish_news"]}'
+        )
+    else:
+        # Если анкеты пользователя в базе нет - предлагаем заполнить
+        await message.answer(
+            text='Вы еще не заполняли анкету. Чтобы приступить - '
+            'отправьте команду /fillform'
+        )
+
+
+@dp.message(StateFilter(default_state))
+async def send_echo(message: Message):
+    await message.reply(text='Извините, моя твоя не понимать')
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
